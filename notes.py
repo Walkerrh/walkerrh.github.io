0a6f19aef5a0b4005e6c70d1cb8c528b1fbec683
Walker Hutchinson <57651882+Walkerrh@users.noreply.github.com>
2023-07-07 00:51:00 -0400

    Create notes.py
    notes file

---
+++ notes.py
@@ -0,0 +1,9 @@
+""" 
+GitHub Pages: This is a free service offered by GitHub. You can host static HTML, CSS, and JavaScript files directly from your GitHub repository.
+
+Netlify: Netlify is a popular choice for deploying websites. It offers a free plan, supports custom domains, and can automatically deploy your website from a Git repository.
+
+Vercel: Vercel is another great option, especially if you're using modern JavaScript frameworks like React, Vue, or Angular. It also offers a free plan and supports custom domains.
+
+Firebase: Firebase is a Google service that offers website hosting along with many other features like a real-time database, authentication, and cloud functions.
+"""
